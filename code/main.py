9fe5ceaa716f5d49db1ce725a2fd224d418a5d23
Dana Goosen <klymentovska@gmail.com>
2025-05-01 18:30:50 +0200

    Create basic setup (map, player, basic collisions)

--- code/main.py
+++ code/main.py
@@ -1,4 +1,6 @@
 from settings import * 
+from sprites import *
+from groups import AllSprites
 
 class Game:
     def __init__(self):
@@ -9,10 +11,23 @@ class Game:
         self.running = True
 
         # groups 
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        
+        # load game
+        self.setup()
 
-
+    def setup(self):
+        tmx_map = load_pygame(join('data', 'maps', 'world.tmx'))
+        for x, y, image in tmx_map.get_layer_by_name('Main').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, (self.all_sprites, self.collision_sprites))
+        for x, y, image in tmx_map.get_layer_by_name('Decoration').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, (self.all_sprites))
+        
+        for obj in tmx_map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                
     def run(self):
         while self.running:
             dt = self.clock.tick(FRAMERATE) / 1000 
@@ -26,7 +41,7 @@ class Game:
 
             # draw 
             self.display_surface.fill(BG_COLOR)
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
         
         pygame.quit()
---
+++ code/sprites.py
@@ -0,0 +1,44 @@
+from settings import *
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft = pos)
+        
+class Player(Sprite):
+    def __init__(self, pos, groups, collision_sprites):
+        surf = pygame.Surface((40,80))
+        super().__init__(pos, surf, groups)
+        
+        # movement & collision
+        self.collision_sprites = collision_sprites
+        self.direction = pygame.Vector2()
+        self.speed = 400
+        
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) -int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) -int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+    
+    def move(self, dt):
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * self.speed * dt
+        self.collision('vertical')
+    
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.rect.left = sprite.rect.right
+                if direction == 'vertical':
+                    if self.direction.y > 0: self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: self.rect.top = sprite.rect.bottom
+                    
+    
+    def update(self, dt):
+        self.input()
+        self.move(dt)
