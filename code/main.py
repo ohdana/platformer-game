4ce5dfe93fdb25f06c6c9dc32fcfc894e37a9268
Dana Goosen <klymentovska@gmail.com>
2025-05-06 18:10:48 +0200

    Add sounds

--- code/main.py
+++ code/main.py
@@ -35,6 +35,7 @@ class Game:
         x = pos[0] + direction * 34 if direction == 1 else pos[0] + direction * 34 - self.bullet_surf.get_width()
         Bullet(self.bullet_surf, (x, pos[1]), direction, (self.all_sprites, self.bullet_sprites))
         Fire(self.fire_surf, pos, self.all_sprites, self.player)
+        self.audio['shoot'].play()
         
     def load_assets(self):
         self.player_frames = import_folder('images', 'player')
@@ -60,11 +61,14 @@ class Game:
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites, self.player_frames, self.create_bullet)
             if obj.name == 'Worm':
                 Worm(self.worm_frames, pygame.FRect(obj.x, obj.y, obj.width, obj.height), (self.all_sprites, self.enemy_sprites))
-    
+
+        self.audio['music'].play(loops = -1)
+        
     def collision(self):
         for bullet in self.bullet_sprites:
             sprite_collision = pygame.sprite.spritecollide(bullet, self.enemy_sprites, False, pygame.sprite.collide_mask)
             if sprite_collision:
+                self.audio['impact'].play()
                 bullet.kill()
                 for sprite in sprite_collision:
                     sprite.destroy()
